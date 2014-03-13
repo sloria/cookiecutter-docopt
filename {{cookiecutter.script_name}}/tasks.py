777b53b07490b052b8557f88ab852ac7d91e6653
Steven Loria <sloria1@gmail.com>
2014-03-12 22:32:39 -0400

    Remove wheel support
    doesn't work well with console scripts

--- {{cookiecutter.script_name}}/tasks.py
+++ {{cookiecutter.script_name}}/tasks.py
@@ -42,12 +42,7 @@ def readme(browse=False):
 @task
 def publish(test=False):
     """Publish to the cheeseshop."""
-    try:
-        __import__('wheel')
-    except ImportError:
-        print("wheel required. Run `pip install wheel`.")
-        sys.exit(1)
     if test:
-        run('python setup.py register -r test sdist bdist_wheel upload -r test')
+        run('python setup.py register -r test sdist upload -r test')
     else:
-        run("python setup.py register sdist bdist_wheel upload")
+        run("python setup.py register sdist upload")
